ff112660d8552df62c8c8ecaa732bedde948fafc
CoderCris <crisamer.ortega@gmail.com>
2024-12-22 13:23:00 +0000

    needle in haystack

--- arrays.hs
+++ arrays.hs
@@ -1,3 +1,5 @@
+import Data.List (elemIndex)
+
 getLargerNumbers :: Ord a => [a] -> [a] -> [a]
 getLargerNumbers arr1 arr2 = map (\(x, y) -> max x y) (zip arr1 arr2)
 
@@ -14,3 +16,11 @@ average list = sum list / fromIntegral (length list)
 
 roundTo2 :: Double -> Double
 roundTo2 x = fromIntegral (round (x * 100)) / 100 
+
+
+
+findNeedle :: [String] -> String
+findNeedle haystack =
+  case elemIndex "needle" haystack of
+    Just index -> "found the needle at position " ++ show index
+    Nothing -> "needle not found"
